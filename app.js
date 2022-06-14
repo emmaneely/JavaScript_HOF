8127d2a0620de4c64f0aa1545e91f483ea8a975a
emmaneely <emmaneely3@gmail.com>
2022-06-14 11:39:40 -0500

    Completed exercise 1

--- app.js
+++ app.js
@@ -2,3 +2,26 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+function plus(number) {
+    return function(plusNumber) {
+        return plusNumber + number;
+    }
+}
+let plus15 = plus(15);
+console.log(plus15(10));
+
+// Exercise 2 Section
+console.log("EXERCISE 2:\n==========\n");
+
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+
+// Exercise 4 Section
+console.log("EXERCISE 4:\n==========\n");
+
+// Exercise 5 Section
+console.log("EXERCISE 5:\n==========\n");
+
+// Exercise 6 Section
+console.log("EXERCISE 6:\n==========\n");

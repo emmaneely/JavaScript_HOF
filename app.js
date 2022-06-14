d0a89887ade0692f7936331f70a6e6d9f140bdfc
emmaneely <emmaneely3@gmail.com>
2022-06-14 11:57:06 -0500

    Completed exercise 2

--- app.js
+++ app.js
@@ -14,6 +14,38 @@ console.log(plus15(10));
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+let users = [
+    {
+        name: "Frodo",
+        age: 50,
+        score: 85,
+        isActive: false,
+    },
+    {
+        name: "Sam",
+        age: 38,
+        score: 94,
+        isActive: true,
+    },
+    {
+        name: "Merry",
+        age: 36,
+        score: 82,
+        isActive: true,
+    },
+    {
+        name: "Pippin",
+        age: 26,
+        score: 77,
+        isActive: false,
+    },
+];
+
+/* users.forEach(function(name) {
+    console.log(name);
+}); */
+users.forEach(name => console.log(name));
+
 // Exercise 3 Section
 console.log("EXERCISE 3:\n==========\n");
 
